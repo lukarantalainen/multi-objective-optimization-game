ab743f61536e3f678df79f1b3dc21a4d44137f56
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-06-15 15:44:58 +0300

    added simple functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,67 @@
 <script lang="ts">
     import Graph from './Graph.svelte';
 
-    let price: number = $state(0);
-    let value2: number = $state(0);
+    let cost: number = $state(0);
+    let comfort: number = $state(0);
     let value3: number = $state(0);
     let value4: number = $state(0);
     let day: number = $state(1);
     let inventory: number = $state(0);
-    let buyers: number = 0;
-    let money: number = $state(0);
+    let money: number = $state(50);
+    let production_cost: number = $state(10);
 
+    let time: number = $state(1);
+    // svelte-ignore state_referenced_locally
+    let delta: number = 1; 
 
-    function increase_day() {
+    let sold: number = $state(0);
+
+    let queue: number = $state(0);
+
+    function handle_interval() {
         day += 1;
-        if (inventory > 0 && buyers > 0) {
+        delta -= 1;
+        sell();
+        check_queue();
+    }
+
+    function sell() {
+        if (inventory > 0) {
             inventory -= 1;
-            buyers -= 1;
-            money += price;
+            money += cost;
+            sold += 1;
         }
-
-        if (price < 50) {
-            buyers += 1;
-        }
-        
     }
 
-    function increment() {
-        inventory += 1;
+    function check_queue() {
+        if (delta == 0) {
+            inventory += 1;
+            delta = time;
+        }
     }
 
-    let dayInterval = setInterval(increase_day, 1000);
+    function make() {
+        if (money < production_cost) {
+            return;
+        }
+        queue += 1;
+        money -= production_cost;
+    }
+
+    let dayInterval = setInterval(handle_interval, 1000);
 </script>
 
 <div class="container">
     <div class="top-bar">
         <div class="left-container">
-            <span>Day {day}</span>
-            <span>Inventory {inventory}</span>
+            <span class="counter">Day {day}</span>
+            <span class="counter">Inventory {inventory}</span>
         </div>
         <div class="stat-container">
-            <span>Sold</span>
-            <span>Customers</span>
-            <span>Money {money}</span>
-            <span>Time</span>
+            <span class="counter">Sold {sold}</span>
+            <span class="counter">Customers</span>
+            <span class="counter">Money {money}</span>
+            <span class="counter">Time {time}</span>
         </div>
         
     </div>
@@ -50,16 +69,26 @@
         <div class="left">
         <h2>Options</h2>
             <div class="options-container">
-                <button onclick={increment}>Make</button>
-                <input type="range" name="weight" id="0" class="slider" bind:value={price}>
-                <span>{price}</span>
-                <input type="range" name="weight" id="1" class="slider" bind:value={value2}>
-                <span>{value2}</span>
-                <input type="range" name="weight" id="2" class="slider" bind:value={value3}>
-                <span>{value3}</span>
-                <input type="range" name="weight" id="3" class="slider" bind:value={value4}>
-                <span>{value4}</span>
+                <div class="option">
+                    <span>Cost {cost} Production cost {production_cost * comfort}</span>
+                    <input type="range" min=10 max=400 name="cost" id="0" class="slider" bind:value={cost}>
+                    
+                </div>
+                <div class="option">
+                    <span>Comfort {comfort}</span>
+                    <input type="range" min=1 max=10 name="comfort" id="1" class="slider" bind:value={comfort}>
+                </div>
+                <div class="option">
+                    <span>{value3}</span>
+                    <input type="range" name="weight" id="2" class="slider" bind:value={value3}>
+                </div>
+                <div class="option">
+                    <span>{value4}</span>
+                    <input type="range" name="weight" id="3" class="slider" bind:value={value4}>
+                </div>
             </div>
+            <button onclick={make}>Make</button>
+            <span>In queue: {queue}</span>
         </div>
         <Graph/>
     </div>
@@ -67,11 +96,6 @@
 </div>
 
 <style>
-    :global(body) {     
-        margin: 0;
-        padding: 0; 
-    }
-
     .content {
         padding: 10px;
         width: inherit;
@@ -84,18 +108,33 @@
         display: flex;
         flex-direction: column;
         gap: 1em;
-
+    }
+    
+    .option {
+        display: flex;
+        justify-content: space-between;
     }
 
     .top-bar {
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        padding: 1em;
+        border-bottom: 1px solid lightgray;
     }
 
     .stat-container {
         display: flex;
         gap: 1em;
     }
-    
+
+    .counter {
+        border-radius: 10px;
+        padding: 4px 10px;
+    }
+
+    .counter:hover {
+        background-color: lightblue;
+    }
 
 </style>
