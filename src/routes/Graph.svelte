8d9679d080aee926d564a7bdd08a7afa304e8b57
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-06-22 15:50:03 +0300

    tried to add a graph

--- src/routes/Graph.svelte
+++ src/routes/Graph.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
   import { money, target, day, finished } from './state';
+  import { Chart } from 'chart.js/auto';
 
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    new Chart(canvas, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+  })
+    
 </script>
 
 <div class="container">
   <div class="progress-container">
     <h2>Progress</h2>
-    <progress value={$money} max={target}></progress>
+    <meter value={$money} max={target}></meter>
+    <p>{$money}/{target   }</p>
 
 
   {#if $finished}
@@ -15,8 +42,9 @@
     </span>
   {/if} 
   </div>
-  
-  
+
+  <canvas bind:this={canvas}></canvas>
+
 </div>
 
 <style>
