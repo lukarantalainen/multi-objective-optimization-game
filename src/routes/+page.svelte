bf0669c32c62fbf4a92dd81f08cd3fcfaa7143da
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-06-26 12:21:18 +0300

    changed layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import Graph from "./Graph.svelte";
     import Button from "./Button.svelte";
-    import Slider from "./Slider.svelte";
+    //import Slider from "./Slider.svelte";
     import { onMount } from "svelte";
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     import { money, target, day, values, spending, revenue } from "./state";
 
     let id: ReturnType<typeof setInterval>;
@@ -139,7 +140,8 @@
             <span class="top-bar-stat">Time cost {time_cost}</span>
         </div>
     </div>
-    <div class="content">
+    <div class="content-container">
+<div class="content">
         <div class="left">
             <div class="options-container">
                 <h2>Options</h2>
@@ -194,17 +196,22 @@
         </div>
         <Graph />
     </div>
-    <footer class="footer">
-        <button class="pause-button" onclick={playPause}>{pauseButtonState}</button>
-    </footer>
+    <button class="pause-button" onclick={playPause}>{pauseButtonState}</button>
+    </div>
+    
+
 </div>
 
 <style>
     :global(html) {
         background-color: rgb(254, 255, 247);
     }
-    .content {
+
+    .content-container {
         padding: 10px;
+    }
+
+    .content {
         width: inherit;
         display: grid;
         grid-template-columns: 1fr 2fr;
@@ -274,103 +281,4 @@
         padding: 4px 5px;
     }
 
-    input[type="range"] {
-        -webkit-appearance: none;
-        margin: 18px 0;
-        width: 200px;
-    }
-    input[type="range"]:focus {
-        outline: none;
-    }
-    input[type="range"]::-webkit-slider-runnable-track {
-        width: 100%;
-        height: 8.4px;
-        cursor: pointer;
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-        background: #3071a9;
-        border-radius: 1.3px;
-        border: 0.2px solid #010101;
-    }
-    input[type="range"]::-webkit-slider-thumb {
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-        border: 1px solid #000000;
-        height: 36px;
-        width: 16px;
-        border-radius: 3px;
-        background: #ffffff;
-        cursor: pointer;
-        -webkit-appearance: none;
-        margin-top: -14px;
-    }
-    input[type="range"]:focus::-webkit-slider-runnable-track {
-        background: #367ebd;
-    }
-    input[type="range"]::-moz-range-track {
-        width: 100%;
-        height: 8.4px;
-        cursor: pointer;
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-        background: #3071a9;
-        border-radius: 1.3px;
-        border: 0.2px solid #010101;
-    }
-    input[type="range"]::-moz-range-thumb {
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-        border: 1px solid #000000;
-        height: 36px;
-        width: 16px;
-        border-radius: 3px;
-        background: #ffffff;
-        cursor: pointer;
-    }
-    input[type="range"]::-ms-track {
-        width: 100%;
-        height: 8.4px;
-        cursor: pointer;
-        background: transparent;
-        border-color: transparent;
-        border-width: 16px 0;
-        color: transparent;
-    }
-    input[type="range"]::-ms-fill-lower {
-        background: #2a6495;
-        border: 0.2px solid #010101;
-        border-radius: 2.6px;
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-    }
-    input[type="range"]::-ms-fill-upper {
-        background: #3071a9;
-        border: 0.2px solid #010101;
-        border-radius: 2.6px;
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-    }
-    input[type="range"]::-ms-thumb {
-        box-shadow:
-            1px 1px 1px #000000,
-            0px 0px 1px #0d0d0d;
-        border: 1px solid #000000;
-        height: 36px;
-        width: 16px;
-        border-radius: 3px;
-        background: #ffffff;
-        cursor: pointer;
-    }
-    input[type="range"]:focus::-ms-fill-lower {
-        background: #3071a9;
-    }
-    input[type="range"]:focus::-ms-fill-upper {
-        background: #367ebd;
-    }
 </style>
